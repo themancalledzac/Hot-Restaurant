a18fe208129d483be8de02d3dd1b7e8c11c1fd9b
Zac Edens <themancalledzac@gmail.com>
2020-12-13 21:48:17 -0800

    server.js now has dependencies for express and path, express handling, and routes to the first two pages.

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+// Dependencies
+// ===========================================
 const express = require('express')
-const app = express()
-
+const path = require('path');
+// express setup
+const app = express();
 var PORT = process.env.PORT || 3001;
 
+// express handling data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// ===========================================
+
+// main page get
 app.get('/', function (req, res) {
-  res.send('Hot Restaurant')
-})
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// tables get
+app.get('/tables', function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
  
 app.listen( PORT )
